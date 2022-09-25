70f34f841e6234675f07a5247a6156a731264a11
Paola Izaguirre <eizaguirre@manconsulting.co.uk>
2022-09-25 14:37:53 -0600

    [IMP] fix styles

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,4 @@ mix.js('resources/js/app.js', 'public/js')
     .vue()
     .postCss('resources/css/app.css', 'public/css', [
         //
-    ])
-    .postCss('resources/js/vue/assets/css/app.css', 'public/css', [
-        //
     ]);
